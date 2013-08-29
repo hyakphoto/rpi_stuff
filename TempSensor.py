0970fb97ba503a22a3d21414318b16e10b48e4c0
Thomas Heuberger <t-h@t-h.cc>
2013-08-29 11:16:11 +0200

    Fixed a bug when the temperatur is negative

--- TempSensor.py
+++ TempSensor.py
@@ -1,6 +1,7 @@
 import smbus
 import time
 import logging
+import struct
 
 class TempSensor:
     """
@@ -39,51 +40,39 @@ class TempSensor:
 
     def __read_sensor(self):         
         """    
-        Gets the temperature data. As the DS1624 is Big-endian and the Pi Little-endian, 
-        the byte order is reversed.
+        Reads the raw temperature data from a DS1624 sensor. 
+                
+        The raw data output is a 16 bit word. 
+        Reading from LEFT to RIGHT, the 1st bit is the MSB and the 13th bit is the LSB, 
+        so 13 bits are payload and the last 3 bits are zeros.
         
-        """            
-
-        """
-        Get the two-byte temperature value. The second byte (endianness!) represents
-        the integer part of the temperature and the first byte the fractional part in terms
-        of a 0.03125 multiplier.
-        The first byte contains the value of the 5 least significant bits. The remaining 3
-        bits are set to zero.
+        When read, because of the endianness differences between the DS1624 and the RPi
+        the byte order is reversed, so the second byte represents the integer part of 
+        the temperature and the first byte the fractional part in terms of a 0.03125 multiplier.      
         """
         return self.bus.read_word_data(self.address, self.DS1624_READ_TEMP)        
-
-    def __convert_raw_to_decimal(self, raw):
-        logging.basicConfig(filename='debug.log', level=logging.DEBUG)
-        logging.debug('raw: ' + str(raw))
-        logging.debug('raw bitstring: ' + bin(raw))
-
-        # Remove the fractional part (first byte) by doing a bitwise AND with 0x00FF
-        temp_integer = raw & 0x00FF
-        logging.debug('integer: ' + str(temp_integer))
-        logging.debug('integer bitstring: ' + bin(temp_integer))
-
-        # Remove the integer part (second byte) by doing a bitwise AND with 0XFF00 and
-        # shift the result bits to the right by 8 places and another 3 bits to the right 
-        # because LSB is the 5th bit          
-        temp_fractional = ((raw & 0xFF00) >> 8) >> 3
-        logging.debug('fractional: ' + str(temp_fractional))
-        logging.debug('fractional bitstring (>>8>>3): ' + bin(temp_fractional))
-        
-        # If the temperature is negative (second byte has a leading 1) ...
-        if (temp_integer & 0x80) == 0x80:            
-            temp_fractional = (~temp_fractional) + 1
-            logging.debug('negative!: ' + str(temp_fractional))
-        return temp_integer + ( 0.03125 * temp_fractional)
     
+    def __convert_temp(self, raw):
+        """
+        Converts the raw data to a decimal value.        
+        """
+        # As the DS1624 is Big-endian and the Pi Little-endian, the byte order is reversed.
+        temp_integer = raw & 0x00FF
+        temp_fractional = ((raw & 0xFF00) >> 8) >> 3
+
+        a, b = struct.unpack('bb', '{}{}'.format(chr(temp_integer), chr(temp_fractional)))
+        return a + (0.03125 * b)
+
     def run_test(self):
+        logging.basicConfig(filename='debug.log', level=logging.DEBUG)
+
         # Examples taken from the data sheet
         values = [0x7D, 0x1019, 0x8000, 0, 0x80FF, 0xF0E6, 0xC9]
 
         for value in values:
-            logging.debug('value: ' + hex(value) + ' result: ' + str(self.__convert_raw_to_decimal(value)))
+            logging.debug('value: ' + hex(value) + ' result: ' + str(self.__convert_temp(value)))
 
     def get_temperature(self):
         self.__send_start();
         time.sleep(0.1);
-        return self.__convert_raw_to_decimal(self.__read_sensor())
+        return self.__convert_temp(self.__read_sensor())
--- TempSensorTest.py
+++ TempSensorTest.py
@@ -7,7 +7,7 @@ def main():
     # logging.disable(logging.CRITICAL)
 
     sensor = TempSensor(0x48)
-    print "%02.02f" % sensor.get_temperature()
+    sensor.get_temperature()
 
 if __name__ == "__main__":
     main()
